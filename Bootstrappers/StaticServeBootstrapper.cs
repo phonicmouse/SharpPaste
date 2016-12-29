41bb265b5d5f00bf3b812b09988832210616bfad
Phonic Mouse <phonicmouse@gmail.com>
2016-12-29 22:32:46 +0100

    add

--- Bootstrappers/StaticServeBootstrapper.cs
+++ Bootstrappers/StaticServeBootstrapper.cs
@@ -15,6 +15,7 @@ namespace SharpPaste
 		protected override void ConfigureConventions(NancyConventions nancyConventions)
 		{
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/css", @"Content"));
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/css", @"Content"));
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/fonts", @"Fonts"));
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/js", @"Scripts"));
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/custom", @"Custom"));
