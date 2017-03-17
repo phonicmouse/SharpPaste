b8c9ccf628f31b20648079e7321f79ad7e6328ed
Phonic Mouse <phonicmouse@gmail.com>
2017-03-17 21:59:53 +0100

    fix

--- Bootstrappers/StaticServeBootstrapper.cs
+++ Bootstrappers/StaticServeBootstrapper.cs
@@ -17,9 +17,9 @@ namespace SharpPaste
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/custom", @"custom")); // Serve custom CSS & JS folder
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/fonts", @"packages\bootstrap.3.3.7\content\fonts")); // Serve bootstrap's fonts folder
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap.css", @"packages\bootstrap.3.3.7\content\Content\bootstrap.min.css")); // Serve Bootstrap CSS
-			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap-flat.css", @"packages\Bootstrap.flat.3.3.4\content\Content\bootstrap-flat.min.css")); // Serve Bootstrap Flat Theme CSS
-			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/bootstrap.js", @"packages\bootstrap.3.3.4\content\scripts\bootstrap.min.js")); // Serve Bootstrap JS
-			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/jquery.js", @"packages\jquery.3.1.1\content\scripts\jquery-3.1.1.min.js")); // Serve jQuery
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap-flat.css", @"packages\Bootstrap.Flat.3.3.4\Content\Content\bootstrap-flat.min.css")); // Serve Bootstrap Flat Theme CSS
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/bootstrap.js", @"packages\bootstrap.3.3.7\content\Scripts\bootstrap.min.js")); // Serve Bootstrap JS
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/jquery.js", @"packages\jQuery.3.1.1\Content\Scripts\jquery-3.1.1.min.js")); // Serve jQuery
 			base.ConfigureConventions(nancyConventions);
 		}
 	}
