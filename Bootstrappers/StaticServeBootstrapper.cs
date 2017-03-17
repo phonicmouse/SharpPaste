d72048a6fea236abd723e1f8d7e41d17c68f00c8
Phonic Mouse <phonicmouse@gmail.com>
2017-03-17 21:58:04 +0100

    wip

--- Bootstrappers/StaticServeBootstrapper.cs
+++ Bootstrappers/StaticServeBootstrapper.cs
@@ -4,8 +4,10 @@
  * Date: 02/08/2016
  * Time: 17:32
  */
+ 
 using Nancy;
 using Nancy.Conventions;
+
 namespace SharpPaste
 {
 	public class StaticServeBootstrapper : DefaultNancyBootstrapper
@@ -13,9 +15,9 @@ namespace SharpPaste
 		protected override void ConfigureConventions(NancyConventions nancyConventions)
 		{
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/custom", @"custom")); // Serve custom CSS & JS folder
-			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/fonts", @"packages\bootstrap.3.3.4\content\fonts")); // Serve bootstrap's fonts folder
-			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap.css", @"packages\bootstrap.3.3.4\content\content\bootstrap.min.css")); // Serve Bootstrap CSS
-			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap-flat.css", @"packages\bootstrap.flat.3.3.4\content\content\bootstrap-flat.min.css")); // Serve Bootstrap Flat Theme CSS
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/fonts", @"packages\bootstrap.3.3.7\content\fonts")); // Serve bootstrap's fonts folder
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap.css", @"packages\bootstrap.3.3.7\content\Content\bootstrap.min.css")); // Serve Bootstrap CSS
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap-flat.css", @"packages\Bootstrap.flat.3.3.4\content\Content\bootstrap-flat.min.css")); // Serve Bootstrap Flat Theme CSS
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/bootstrap.js", @"packages\bootstrap.3.3.4\content\scripts\bootstrap.min.js")); // Serve Bootstrap JS
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/jquery.js", @"packages\jquery.3.1.1\content\scripts\jquery-3.1.1.min.js")); // Serve jQuery
 			base.ConfigureConventions(nancyConventions);
