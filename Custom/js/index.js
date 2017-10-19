cbf161707fd676e02c44f587867598393ced32ec
Phonic Mouse <phonicmouse@gmail.com>
2017-10-20 00:57:15 +0100

    still working...do not use at this point

--- Custom/js/index.js
+++ Custom/js/index.js
@@ -27,19 +27,36 @@ var spinnerTarget = document.getElementById("spinner");
 $("#addpaste").click(function () {
     if ($.trim($("#body").val())) {
         var spinner = new Spinner(opts).spin(spinnerTarget);
-        var paste = {};
+        var title = null;
         if ($.trim($("#title").val())) {
-            paste.title = $("#title").val();
+            title = $("#title").val();
         } else {
-            paste.title = "Untitled";
+            title = "Untitled";
         }
-        paste.body = $("#body").val();
-        paste.language = $("#languageSelection option:selected").val();
-        var data = JSON.stringify(paste);
-        console.log("JSON:", data);
-        $.post("/add", data, function (responseData) {
-            var pasteToken = new String(responseData);
-            window.location = location.origin + "/" + pasteToken;
+        var body = $("#body").val();
+        var language = $("#languageSelection option:selected").val();
+        var pwLength = 32;
+        var saltLength = 32;
+        var pw = new buffer.SlowBuffer(generatePassword(pwLength).normalize('NFKC'));
+        var salt = new buffer.SlowBuffer(generatePassword(saltLength).normalize('NFKC'));
+        var n = 1024, r = 8, p = 1;
+        var dkl = 32;
+        scrypt(pw, salt, n, r, p, dkl, function (error, progress, key) {
+            if (error) {
+                console.log("Error: " + error);
+            } else if (key) {
+                var aes = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(23));
+                var encryptedPaste = {};
+                encryptedPaste.title = aesjs.utils.hex.fromBytes(aes.encrypt(aesjs.utils.utf8.toBytes(title)));
+                encryptedPaste.body = aesjs.utils.hex.fromBytes(aes.encrypt(aesjs.utils.utf8.toBytes(body)));
+                encryptedPaste.language = aesjs.utils.hex.fromBytes(aes.encrypt(aesjs.utils.utf8.toBytes(language)));
+                var data = JSON.stringify(encryptedPaste);
+                console.log("JSON:", data);
+                $.post("/add", data, function (responseData) {
+                    var pasteToken = new String(responseData);
+                    window.location = location.origin + "/" + pasteToken + "#" + btoa(aesjs.utils.hex.fromBytes(key));
+                });
+            }
         });
     } else {
         $("#pasteerrormodal").modal("show");
