f4a69e276802a15dc3398ac13156ace9bfb1502b
Phonic Mouse <phonicmouse@gmail.com>
2017-10-20 16:38:14 +0100

    n

--- Custom/js/index.js
+++ Custom/js/index.js
@@ -1,32 +1,8 @@
-﻿/* Spinner */
-var opts = {
-    lines: 17 // The number of lines to draw
-    , length: 56 // The length of each line
-    , width: 2 // The line thickness
-    , radius: 83 // The radius of the inner circle
-    , scale: 1 // Scales overall size of the spinner
-    , corners: 1 // Corner roundness (0..1)
-    , color: '#000' // #rgb or #rrggbb or array of colors
-    , opacity: 0 // Opacity of the lines
-    , rotate: 0 // The rotation offset
-    , direction: 1 // 1: clockwise, -1: counterclockwise
-    , speed: 1.6 // Rounds per second
-    , trail: 60 // Afterglow percentage
-    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-    , zIndex: 2e9 // The z-index (defaults to 2000000000)
-    , className: 'spinner' // The CSS class to assign to the spinner
-    , top: '50%' // Top position relative to parent
-    , left: '50%' // Left position relative to parent
-    , shadow: false // Whether to render a shadow
-    , hwaccel: true // Whether to use hardware acceleration
-    , position: 'absolute' // Element positioning
-}
-var spinnerTarget = document.getElementById("spinner");
+﻿/* Graphics */
 
 /* Paste Encryption and Sending */
 $("#addpaste").click(function () {
     if ($.trim($("#body").val())) {
-        var spinner = new Spinner(opts).spin(spinnerTarget);
         var title = null;
         if ($.trim($("#title").val())) {
             title = $("#title").val();
