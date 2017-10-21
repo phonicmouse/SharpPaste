c5fbeca3e89b24813fc39725cefc3816ef3e76a0
Phonic Mouse <phonicmouse@gmail.com>
2017-10-21 01:44:06 +0100

    changed dirs

--- StaticServeBootstrapper.cs
+++ StaticServeBootstrapper.cs
@@ -16,10 +16,10 @@ namespace SharpPaste
 		{
             nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap.css", @"packages\bootstrap.3.3.7\content\Content\bootstrap.min.css")); 
             nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/css/bootstrap-flat.css", @"packages\Bootstrap.Flat.3.3.4\Content\Content\bootstrap-flat.min.css")); 
-            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/bootstrap.js", @"packages\bootstrap.3.3.7\content\Scripts\bootstrap.min.js")); 
-            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/jquery.js", @"packages\jQuery.3.2.1\Content\Scripts\jquery-3.2.1.min.js")); 
-            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/js", @"Custom\js")); 
-			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/css", @"Custom\css")); 
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/lib/bootstrap.js", @"packages\bootstrap.3.3.7\content\Scripts\bootstrap.min.js")); 
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddFile("/js/lib/jquery.js", @"packages\jQuery.3.2.1\Content\Scripts\jquery-3.2.1.min.js")); 
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/js", @"Static\js")); 
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/css", @"Static\css")); 
 			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/fonts", @"packages\bootstrap.3.3.7\content\fonts")); 
 			base.ConfigureConventions(nancyConventions);
 		}
