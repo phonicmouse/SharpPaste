cc0cc26c43289c0c996e13fec6a24642dade969b
Phonic Mouse <phonicmouse@gmail.com>
2017-10-21 01:30:29 +0100

    changes pasted db location

--- Config.cs
+++ Config.cs
@@ -1,16 +1,10 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Phonic Mouse
- * Date: 02/08/2016
- * Time: 15:16
- */
-using System;
+﻿using System;
 
 namespace SharpPaste
 {
 	public class Config
 	{
-		public static string DBPATH = string.Format(@"{0}Pastes.db", AppDomain.CurrentDomain.BaseDirectory);
+		public static string DBPATH = string.Format(@"{0}pastes.db", AppDomain.CurrentDomain.BaseDirectory);
 
 		public static int TOKENLENGTH = 23;
 	}
