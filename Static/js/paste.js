848a0d88e56c3e23e82f7d9f119420d2a683ee62
Phonic Mouse <phonicmouse@gmail.com>
2017-10-22 20:32:36 +0100

    some fixes

--- Static/js/paste.js
+++ Static/js/paste.js
@@ -1,8 +1,11 @@
-﻿var key = aesjs.utils.hex.toBytes(atob(String(location).substring(String(location).lastIndexOf('#') + 1)));
-var aes = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(23));
-$.getJSON(window.location.protocol + "/json" + window.location.pathname, function (data) {
+﻿$.getJSON(window.location.protocol + "/json" + window.location.pathname, function (data) {
+    var key = aesjs.utils.hex.toBytes(atob(String(location).substring(String(location).lastIndexOf('#') + 1)));
+    var aes = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(23));
     $("#title").text(aesjs.utils.utf8.fromBytes(aes.decrypt(aesjs.utils.hex.toBytes(data.Title))));
     document.title = 'SharpPaste - ' + $("#title").text();
     $("#body").text(aesjs.utils.utf8.fromBytes(aes.decrypt(aesjs.utils.hex.toBytes(data.Body))));
     $("#body").addClass('language-' + aesjs.utils.utf8.fromBytes(aes.decrypt(aesjs.utils.hex.toBytes(data.Language))));
+    $.getScript("/js/lib/prism.js", function () {
+        $("#codeblock").removeAttr('hidden');
+    });
 });
