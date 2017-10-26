4f9f633427a051dbce2877f1ffd110685745c9f3
Phonic Mouse <phonicmouse@gmail.com>
2017-10-26 16:09:50 +0100

    test 1

--- Deployment/develop.sh
+++ Deployment/develop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo 'Deploying to beta server...'
 echo 'Successfully deployed.'
+exit 0
--- Deployment/master.sh
+++ Deployment/master.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo 'Delpying to live server...'
 echo 'Successfully deployed.'
+exit 0
