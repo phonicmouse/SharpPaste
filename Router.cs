825c5b00be083c7291aca81e11e0f79bc09d08d3
Phonic Mouse <phonicmouse@gmail.com>
2017-10-27 00:18:09 +0100

    final fixes

--- Router.cs
+++ Router.cs
@@ -19,7 +19,7 @@ namespace SharpPaste
                 {
                     var collection = db.GetCollection<Paste>("pastes");
                     var paste = collection.FindOne(Query.EQ("LongId", parameters.longId.ToString()));
-                    if (paste == null) return View["paste"];
+                    if (paste == null) return View["404"];
                     paste.WebViews += 1;
                     collection.Update(paste);
                     return View["paste", paste];
